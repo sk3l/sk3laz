ecbbe4278dfbaea987a0be448414f5cfeaa33779
mskelton <skelton.michael@gmail.com>
2023-04-12 07:38:31 -0700

    Rework destroy-resource.sh

--- destroy-resource.sh
+++ destroy-resource.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Usage
+# az resource delete [--api-version]
+#   [--ids]
+#   [--latest-include-preview]
+#   [--name]
+#   [--namespace]
+#   [--no-wait]
+#   [--parent]
+#   [--resource-group]
+#   [--resource-type]
+
+# Documentation here:
+# https://learn.microsoft.com/en-us/cli/azure/resource?view=azure-cli-latest#az-resource-delete
+
 az resource delete \
-	--id $1
+	--ids $1
